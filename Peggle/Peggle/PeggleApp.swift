358c4d49bbd4e29b41bf97ed9bef00ebfdbc9b75
Lee Hern Ping <hpingleenet@gmail.com>
2024-01-24 10:43:44 +0800

    Initial commit

---
+++ Peggle/Peggle/PeggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeggleApp.swift
+//  Peggle
+//
+//  Created by proglab on 24/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PeggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
